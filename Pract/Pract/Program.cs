7c2efe76760e465a33687bfcb427a977566e8ed0
paresh218 <paresh218@gmail.com>
2018-08-27 16:13:34 +0530

    Add files via upload

---
+++ Pract/Pract/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract
+{
+    public class Employee
+    {
+        public string FirstName;
+        public string LastName;
+
+        public void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
+    public class PartTimeEmployee : Employee
+    {
+        public new void PrintFullName()
+        {
+            Console.WriteLine(FirstName + " " + LastName + "-contractor");
+            //base.PrintFullName();
+        }
+    }
+    public class FullTimeEmployee : Employee
+    {
+
+    }
+
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            FullTimeEmployee FTE = new FullTimeEmployee();
+            FTE.FirstName = "FullTime";
+            FTE.LastName = "Employee";
+            FTE.PrintFullName();
+
+            PartTimeEmployee PTE = new PartTimeEmployee();
+            PTE.FirstName = "PartTime";
+            PTE.LastName = "Employee";
+            PTE.PrintFullName();
+            //((Employee)PTE).PrintFullName();
+
+            //Employee E = new PartTimeEmployee();
+            //E.PrintFullName();
+
+            
+            Console.ReadLine();
+        }
+    }
+
+}
